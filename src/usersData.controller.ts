961b655013edf0fe265612e60cac5c7084201d1c
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2023-01-25 03:34:09 +0600

    usersData

--- src/usersData.controller.ts
+++ src/usersData.controller.ts
@@ -9,10 +9,15 @@ import {
   Body,
 } from '@nestjs/common';
 
+interface videoDTO {
+  name: string;
+  tag: string;
+}
+
 @Controller('/users')
 export class UserDataController {
   @Post('/video')
-  getProfile(@Body() requestData: Record<string, any>) {
+  getProfile(@Body() requestData: videoDTO) {
     console.log(requestData);
     return { success: true };
   }
