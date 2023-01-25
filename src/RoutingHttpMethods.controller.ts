b2bc77c7c9238d5a2db768789d816da7398d4657
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2023-01-25 08:48:28 +0600

    issueSolve

--- src/RoutingHttpMethods.controller.ts
+++ src/RoutingHttpMethods.controller.ts
@@ -1,32 +1,57 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
+  Param,
   Post,
   Put,
-  Delete,
-  Patch,
-  Req,
-  Body,
-  Param,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto';
 
-const USERS = [];
+let USERS = [];
 
 @Controller('/users')
 export class RoutingHttpController {
   @Post()
-  addUser(@Body() createUserDto: CreateUserDTO) {
+  createUser(@Body() createUserDto: CreateUserDTO) {
     USERS.push(createUserDto);
-    console.log(createUserDto);
-    return 'users added';
+    return { message: 'User created' };
   }
+
   @Get()
-  getUser() {
+  findAllUsers() {
     return USERS;
   }
+
   @Get(':id')
-  getUser(@Param(':id') id: number) {
-    return USERS.find((user) => user.id === +id);
+  findUserById(@Param('id') id: number) {
+    const user = USERS.find((user) => user.id === +id);
+
+    if (!user) {
+      return { message: 'User not found' };
+    }
+
+    return user;
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() updateUserDto: CreateUserDTO) {
+    const userIdx = USERS.findIndex((user) => user.id === +id);
+
+    if (!userIdx) {
+      return { message: 'User not found' };
+    }
+
+    USERS[userIdx] = updateUserDto;
+
+    return { message: 'User updated' };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    USERS = USERS.filter((user) => user.id !== +id);
+
+    return { message: 'User deleted' };
   }
 }
